b79a5d25bacc9e99987bad384e00aa887b07ece6
Ryan Burns <ryankburns@gmail.com>
2021-12-17 17:21:50 -0800

    added CRUD methods for contractors

--- app/controllers/contractors_controller.rb
+++ app/controllers/contractors_controller.rb
@@ -1,2 +1,28 @@
 class ContractorsController < ApplicationController
+
+    def new
+        contractor = Contractor.new
+        contractor.build_user
+    end
+
+    def create
+        contractor = Contractor.new(client_params)
+            if contractor.save
+                user = contractor.user
+                session[:user_id] = user.id
+                    render json: contractor, status: 200
+            else
+                response = {
+                    error: contractor.errors.full_messages.to_sentence
+                }
+                render json: response, status: unprocessable_entity
+            end
+    end
+
+    private
+
+    def client_params
+        params.require(:contractor).permit(:name, user_attributes: [:email, :password])
+    end
+    
 end
