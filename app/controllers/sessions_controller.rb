08f73fe089867de2cef95ba895bb78daf4ab2afe
Ryan Burns <ryankburns@gmail.com>
2021-12-17 16:55:03 -0800

    added methods to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,40 @@
 class SessionsController < ApplicationController
+
+    def create
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: current_user, status: 200
+        else
+            render json: {
+                error: "Invalid Credentials."
+            }
+        end
+    end
+
+    def get_current_user
+        if logged_in?
+            render json: current_user, status: 200
+        else
+            render json: {
+                error: "No one logged in."
+            }
+        end
+    end
+
+    def destroy
+        session.delete :user_id
+            if !session[:user_id]
+                render json: {
+                    notice: "You have successfully logged out."
+                }, status: 200
+            else
+                render json: {
+                    error: "Unsuccessful log out."
+                }
+            end
+    end
+
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+    def new
+        user = User.new
+        render json: user, status: 200
+    end
+
+
+    private
+
+    def user_params
+        params.permit(:username, :password)
+    end
+
 end
