99c21d05af82b423853f79c8b4242b3083b5dd3c
Ryan Burns <ryankburns@gmail.com>
2021-12-22 22:13:03 -0800

    changed json render

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
         user = User.find_by(email: params[:email])
         if user && user.authenticate(params[:password])
             session[:user_id] = user.id
-            render json: user, status: 200
+            render json: current_user, status: 200
         else
             render json: {
                 error: "Invalid Credentials."
